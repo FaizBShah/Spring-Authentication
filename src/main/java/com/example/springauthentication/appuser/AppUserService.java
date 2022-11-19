729fa24c568922c73f772d1402c1dd556a7fc8dd
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2022-11-19 21:17:33 +0530

    Created the logic to register user

--- src/main/java/com/example/springauthentication/appuser/AppUserService.java
+++ src/main/java/com/example/springauthentication/appuser/AppUserService.java
@@ -1,5 +1,6 @@
 package com.example.springauthentication.appuser;
 
+import com.example.springauthentication.security.PasswordEncoder;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,11 +14,28 @@ public class AppUserService implements UserDetailsService {
     private static final String USER_NOT_FOUND_MSG = "user with email %s not found";
 
     private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email).orElseThrow(
                     () -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)
                 ));
+    }
+
+    public String signUpUser(AppUser appUser) {
+        boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
+
+        if (userExists) {
+            throw new IllegalStateException("Email already taken");
+        }
+
+        String encodedPassword = passwordEncoder.bCryptPasswordEncoder().encode(appUser.getPassword());
+
+        appUser.setPassword(encodedPassword);
+
+        appUserRepository.save(appUser);
+
+        return "It works";
     }
 }
